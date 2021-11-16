c17b382c389a60b83787e5541829c25ff821b330
mw3981 <mw3981@nyu.edu>
2021-11-16 08:35:20 -0500

    Importing pages and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+// Pages import
+import Login from "./pages/Login";
+import CreateUser from "./pages/CreateUser";
+import UserProfile from "./pages/UserProfile";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    // fragment bc React only allows you to return a single element
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/user/:id" element={<UserProfile />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
